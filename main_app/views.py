60c3aee18a9c96974fc568eb29caa32c7817517b
User Name <redd@Gilbertos-MacBook-Pro.local>
2024-01-13 14:16:39 -0600

    added urls and templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def subpost_detail(request, subpost_name):
+    return render(request, 'subpost_detail.html', {'subpost_name': subpost_name})
+
+def post_detail(request, post_id):
+    return render(request, 'post_detail.html', {'post_id': post_id})
+
