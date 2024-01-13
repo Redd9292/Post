011f97eb15b3cc04586971bd50180c889de65feb
User Name <redd@Gilbertos-MacBook-Pro.local>
2024-01-13 15:41:08 -0600

    created post forum

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from .import views
+from .views import create_post
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('subpost/<slug:subpost_name>/', views.subpost_detail, name='subpost_detail'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('create_post/', create_post, name='create_post'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 
 def home(request):
     return render(request, 'home.html')
@@ -9,3 +10,14 @@ def subpost_detail(request, subpost_name):
 def post_detail(request, post_id):
     return render(request, 'post_detail.html', {'post_id': post_id})
 
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'create_post.html', {'form': form})
